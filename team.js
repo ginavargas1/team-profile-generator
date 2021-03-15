58916745dc7bf1bf38c87cc169c1f75bbe3827d2
Gina Vargas <gina.vargas89@outlook.com>
2021-03-15 09:53:13 -0700

    testing clears

--- team.js
+++ team.js
@@ -1,166 +1,114 @@
 // node modules 
-const inquirer = require ("inquirer");
+const inquirer = require("inquirer");
 const fs = require("fs")
 
 // constructors
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const Employee = require ("./lib/Employee");
+const Employee = require("./lib/Employee");
 
 const employees = [];
 
-function myTeam(){
+function myTeam() {
     teamAssemble();
-    htmlPage();
+    // htmlPage();
 }
 
-function teamAssemble(){
-   inquirer.prompt([ 
-    {
-        type: 'list',
-        name: 'role',
-        message: 'What is your role?',
-        choices: ['Manager', 'Intern', 'Engineer', 'New Role'],
-    },
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is your ID number?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?'
-    }
-  
-  ])
-
-
-    .then(function({role, name, id, email}) {
-        let roleInfo = "";
-        // if/else statement to roles
-        if(role === 'Engineer') {
-            roleInfo = 'Github username';
-        } else if (role === 'Intern') {
-            roleInfo = 'School Name';
-        } else {
-            roleInfo = 'Office Phone Number'
+// employee questions 
+function teamAssemble() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is your role?',
+            choices: ['Manager', 'Intern', 'Engineer'],
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your ID number?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
         }
 
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'roleInfo',
-                message: `Enter team members ${roleInfo}`
-            },
-            {
-                type:'list',
-                name: 'addMembers',
-                massage: 'Would you like to additional members?',
-                choices: ['Yes', 'No']
-            }
+    ])
 
-        ]
-        )
-
-        .then(function({roleInfo, addMembers}) {
-            let teamMembers;
+        // adding roles
+        .then(function ({ role, name, id, email }) {
+            let roleInfo = "";
+            // if/else statement to roles
             if (role === 'Engineer') {
-                teamMembers = new Engineer (name, id, email, roleInfo);
-            }else if (role === 'Intern') {
-                teamMembers = new Intern (name, id, email, roleInfo);
-            }else {
-                teamMembers = new Manager (name, id, email, roleInfo);
+                roleInfo = 'Github username';
+            } else if (role === 'Intern') {
+                roleInfo = 'School Name';
+            } else {
+                roleInfo = 'Office Phone Number'
             }
 
-            employees.push(teamMembers);
-            addHtml(teamMembers)
-            .then(function(){
-                if(addMembers === 'Yes'){
-                    teamAssemble();
-                }else {
-                    completeHtml();
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'roleInfo',
+                    message: `Enter team members ${roleInfo}`
+                },
+                {
+                    type: 'list',
+                    name: 'addMembers',
+                    massage: 'Would you like to additional members?',
+                    choices: ['Yes', 'No']
                 }
-            });
+
+            ]
+            )
+
+                //adding new members
+                .then(function ({roleInfo, addMembers}) {
+                    let teamMembers;
+
+                    console.table(roleInfo)
+                    if (role === 'Engineer') {
+                        teamMembers = new Engineer(name, id, email, roleInfo);
+                        employees.push(teamMembers)
+                    } else if (role === 'Intern') {
+                        teamMembers = new Intern(name, id, email, roleInfo);
+                        employees.push(teamMembers)
+                    } else if (role === "Manager") {
+                        teamMembers = new Manager(name, id, email ,roleInfo);
+                        employees.push(teamMembers)
+                    };
+
+                    if (data.addMembers == "Yes") {
+                        teamAssemble()
+
+                    }// if they say no then html is ready
+                    //html - employees loop 
+                    // each role will need to be defined
+                    // info will have to populate in html (js append OR FS writefile)
+
+
+                    console.log(teamMembers);
+                });
+
         });
 
-    });
-
 }
 
-// html page 
-function htmlPage() {
-    const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style.css">
-    <title>Team Profile Generator</title>
-    </head>
-    <body>
-    <div class="jumbotron text-center">
-        <div class="container">
-          <h1 class="display-4">My Team</h1>
-        </div>
-      </div>
-    
-     <div class="container">
-        <div class="row">`;
-    
-    fs.writeFile('/index.html', html, function(err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-    
-    console.log('start');
+// // html page 
+// function htmlPage() {
+//     const html = 
 
 
-}
-
-function addHtml(team) {
-    return new Promise (function(resolve, reject) {
-        const name = member.getName();
-        const role = member.getRole();
-        const id = member.getId();
-        const email = member.getEmail();
-        let data = '';
-
-        if (role=== 'Engineer') {
-            const gitHub = member.getGitHub();
-            data = `
-            <div class="row row-cols-1 row-cols-md-3 g-4">
-                <div class="col">
-                <div class="card h-100">
-                <div class="card-body">
-                <h5 class="card-header">Engineer<br /><br />${name}</h5>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">GitHub: ${gitHub}</li>
-            </ul>
-            </div>
-            </div>
-            </div>
-            `;
-        } else if (role ==='Intern')
-            
-    })
-}
 
 
- 
 //   .catch(error => {
 //     console.log(error)
 //     if(error.isTtyError) {
